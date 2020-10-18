70337de08efbbd36e149ee3b7d0618881599e137
Rudy Ailabouni <eilabouni.rudy@gmail.com>
2020-10-18 17:30:21 +0200

    basic server setup

---
+++ server/server.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+from resources.item import Item
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+api.add_resource(Item, '/item', '/item/<string:item_id>', endpoint='item')
+
+
+
+if __name__ == '__main__':
+    # The following line commented out line makes the server accessible from other PCs in the same network at port 80
+    # app.run(host='0.0.0.0', port=80, debug=True)
+    # Runs at localhost:5000 
+    app.run(debug=True)
---
+++ server/utils/errors.py
@@ -0,0 +1,14 @@
+def error_response(error: Exception or str, status=500, toConsole=True): 
+    """creates a generic error response tuple with the error message and status 500 
+
+    Args:
+        error (Exception or str): Either the actual exception class or an error string that will be returned
+        status (int, optional): The HTTP status code to be returned with the error. Defaults to 500.
+        toConsole (bool, optional): Whether to also print the error to the console. Defaults to True.
+
+    Returns:
+        tuple(dict(str,str), int): A tuple that looks like ({"error": <errorMsg}, <HttpStatusCode>)
+    """
+    if (toConsole):
+        print(error)
+    return {"error": str(error)}, status
