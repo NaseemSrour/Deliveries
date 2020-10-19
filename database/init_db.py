e8407ce214d2295286b3b913f919b7eaf7acbe93
Naseem Srour <naseemsr94@gmail.com>
2020-10-19 03:32:00 +0300

    added creatng the DB if it doesn't exist

--- database/init_db.py
+++ database/init_db.py
@@ -11,6 +11,30 @@ dbConfig = {
 }
 
 
+# Create DB if it doesn't exist
+initial_db_config = {
+  'user': config.db_username,
+  'password': config.db_password,
+  'host': config.db_host, 'autocommit':True
+}
+
+mydb = mysql.connector.connect(**initial_db_config)
+mycursor = mydb.cursor()
+mycursor.execute("SHOW DATABASES")
+for x in mycursor:
+    if x == config.db_name:
+      print("Database '" + config.db_name +"' already exists"
+    else:
+        print("Database '" + config.db_name +"' doesn't exit, creating it..")
+        mycursor.execute("CREATE DATABASE " + config.db_name)
+
+mycursor.close()
+mydb.close()
+
+
+
+# Create tables
+print("Creating tables...")
 db = mysql.connector.connect(**dbConfig)
 cursor = db.cursor()
 
@@ -18,6 +42,6 @@ cursor = db.cursor()
 for line in open(config.create_all_tables_path):
     cursor = db_controller.execute_query(cursor, query=line)
     
-
+print("Tables created succesfully!")
 cursor.close()
 db.close()
