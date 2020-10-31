af34ec066a1957e782b8a793670cb1c9735949b4
Naseem Srour <naseemsr94@gmail.com>
2020-11-01 01:51:18 +0200

    Used the Item Resource to fulfill a GET request from server.py, by using the db_controller.py to fetch from database.

--- server/database/db_controller.py
+++ server/database/db_controller.py
@@ -32,3 +32,32 @@ def execute_query(cursor, query, data=None):
         else:
             print("Executing query " + query + " failed: {}".format(err))
         return None
+
+
+def get_item(item_id):
+    select_query = "SELECT item_name, item_desc, price, business_id FROM Item WHERE item_id=" + item_id + ";"
+    db = mysql.connector.connect(**dbConfig)
+    cursor = db.cursor()
+    cursor = execute_query(cursor, select_query)
+    row = cursor.fetchone()
+    if(row is not None):
+        cursor.close()
+        db.close()
+        # Suggestion: Use here the DBItem class to create an object and save the data (row) in it, and then return this object instead of returning 'row' as a tuple.
+        return row
+    else:
+        return("Item with ID " + item_id + " + is not found!")
+
+
+    
+def add_item(item):
+    item = json.loads(item)
+    insert_item_query = "INSERT INTO Item(item_name, item_desc, image, price, business_id) VALUES('{}', '{}', {}, '{}', '{}');".format(item["item_name"], item["item_desc"], "load_file('" + item["image_path"] + "')", item["price"], item["business_id"])
+    
+    db = mysql.connector.connect(**dbConfig)
+    cursor = db.cursor()
+    cursor = execute_query(cursor, insert_item_query)
+    cursor.close()
+    db.close()
+
+    return "Success"
--- server/resources/item.py
+++ server/resources/item.py
@@ -1,23 +1,24 @@
 import sys
-sys.path.append('C:\\Users\\Naseem\\Desktop\\Deliveries')
+sys.path.append('C:\\Users\\Naseem\\Desktop\\Deliveries\\')
 
 from flask import request
 from flask_restful import Resource, reqparse
 # from utils.errors import error_response
-from server.models.db_entities import DBItem
-from server.database import db_controller
+from models.db_entities.db_item import DBItem
+from database import db_controller
 
 class Item(Resource):
-
-    def get(self, item_id: str = None):
+    def get(self, item_id=None):
         """Retrives the item with the provided ID"""
         try:
             # TODO:  actual logic to be added once Database connector is available
             if (item_id is not None):
-                return f"Item with ID {item_id} retrived"
+                retrieved_item = db_controller.get_item(item_id)
+                return retrieved_item
+                # Further to-do: handling a successful response properly in Flask Restful
             return "Item Retrieved", 200  
         except Exception as err:
-            return error_response(err)
+            return str(err)
 
     def post(self):
         """Created a new item"""
