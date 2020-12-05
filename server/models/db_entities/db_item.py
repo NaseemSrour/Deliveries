9af3a251c9dcfd716c1e9733b95cbecdd7624fb3
Naseem Srour <naseemsr94@gmail.com>
2020-12-05 22:02:46 +0200

    Added & tested BLOBs handling in DBItem class

--- server/models/db_entities/db_item.py
+++ server/models/db_entities/db_item.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath
 import logging
 
 import database.db_controller as db_controller
+import utils.binary_file_ops as file_ops
 
 logger = logging.getLogger(__name__)
 
@@ -18,19 +19,20 @@ class DBItem:
         self.price = price
         self.business_id = business_id
 
-    ##### MISSING: BLOBs handling (the 'image' column)
+
     def add_item(self):
-        insert_item_query = "INSERT INTO Item(item_name, item_desc, price, business_id) VALUES(%s, %s, %s, %s)"
-        data = (self.name, self.desc, self.price, self.business_id)
+        insert_item_query = "INSERT INTO Item(item_name, item_desc, image, price, business_id) VALUES(%s, %s, %s, %s, %s)"
+        data = (self.name, self.desc, self.image, self.price, self.business_id)
 
         result = db_controller.execute_query(insert_item_query, data)  # 'result' should be equal to None on success.
         logger.info("Item " + self.name + " added successfully.")
 
-    ##### Missing: BLOBs handling (the 'image' column)
+
     def update_item(self):
-        update_query = "UPDATE item SET item_name=%s, item_desc=%s, price=%s WHERE item_id=%s AND business_id=%s"
-        data = (self.name, self.desc, self.price, self.ID, self.business_id)
+        update_query = "UPDATE item SET item_name=%s, item_desc=%s, image=%s, price=%s WHERE item_id=%s AND business_id=%s"
+        data = (self.name, self.desc, self.image, self.price, self.ID, self.business_id)
         db_controller.execute_query(update_query, data)  # returns None
+
 
     def delete_item(self):
         delete_statement = "DELETE FROM item WHERE item_id=%s AND business_id=%s"
@@ -39,19 +41,20 @@ class DBItem:
         
     
     def __str__(self):
+        # Print without the image bytes.
         return str([self.ID, self.name, self.desc, self.price, self.business_id])
 
 
-##### Missing: BLOBs handling
+
 # Returns: DBItem instance, or None on failure.
 def get_item(item_id: int) -> DBItem:
     if(type(item_id) != int):  # MAYBE WE ALSO PREVENT USER FROM INPUTING ANYTHING OTHER THAN A NUMBER (in the  web forms + in the HTTP requests).
         err_msg = "item_id must be int!"
         logger.error(err_msg)
-        # TODO: @Drops - the return here should be removed, or at least return a DBItem somehow. Leaving this here for you. 
+        # TODO: @Drops - the return here should be removed, or at least return a DBItem somehow. Leaving this here for you.
         return(err_msg)
     
-    select_query = "SELECT item_id, item_name, item_desc, price, business_id FROM Item WHERE item_id=%s"  # This way is called binding params, which is a safe way to prevent a SQL injection attack - because MySQL makes sure the value is of the correct type.
+    select_query = "SELECT item_id, item_name, item_desc, image, price, business_id FROM Item WHERE item_id=%s"  # This way is called binding params, which is a safe way to prevent a SQL injection attack - because MySQL makes sure the value is of the correct type.
     data = (item_id,) # a comma endicates a Tuple
     results = db_controller.execute_query(select_query, data) # Returns a list of tuples; in this case, should be one tuple.
     
@@ -62,7 +65,7 @@ def get_item(item_id: int) -> DBItem:
             logger.error(err_msg)
             return(err_msg)
         
-        retrieved_item = DBItem(res_item[0], res_item[1], res_item[2], None, res_item[3], res_item[4]) # Indexes are according to the SELECT statement above.
+        retrieved_item = DBItem(res_item[1], res_item[2], res_item[3], res_item[4], res_item[5], res_item[0]) # Indexes are according to the SELECT statement above.
         return retrieved_item
     
     else:
@@ -71,21 +74,20 @@ def get_item(item_id: int) -> DBItem:
         return(err_msg)
 
 
-##### MISSING: BLOBs handling
 # Returns: list of DBItem instances, or None at failure.
 def get_items(businessID: int) -> list[DBItem]:
     if(type(businessID) != int):
         err_msg = "business_id must be int!"
         logger.error(err_msg)
-        return(err_msg)
-    select_query = "SELECT item_name, item_desc, price, business_id FROM item WHERE item.business_id=%s"
+        return (err_msg)
+    select_query = "SELECT item_id, item_name, item_desc, image, price, business_id FROM item WHERE item.business_id=%s"
     data = (businessID,)
     results = db_controller.execute_query(select_query, data)  # returns a list of tuples
 
     db_items_lst = []
     if(results is not None and len(results) > 0):
         for row in results:
-            retrieved_item = DBItem(row[0], row[1], None, row[2], row[3])
+            retrieved_item = DBItem(row[1], row[2], row[3], row[4], row[5], row[0])
             db_items_lst.append(retrieved_item)
         return db_items_lst
     else:
@@ -93,19 +95,33 @@ def get_items(businessID: int) -> list[DBItem]:
         return db_items_lst  # We can eliminate the if-else and just always return db_items_lst, but for now I want to keep the print statement when the above IF condition fails.
 
 
-def test():
-    new_item = DBItem("lafe kbeere", "lafet 5nzeer kbere'3", None, 25, 1)
-    logger.info(new_item.name + "'s price is: " + str(new_item.price) + ", it contains: " + new_item.desc)
+# ---------------------------
+
+# Local stam tests:
+def test_add_item():
+    myimgdata = file_ops.convertToBinaryData('C:\\Users\\Naseem\\Desktop\\Untitled.png')
+
+    new_item = DBItem("sala6a", "Tabouli", myimgdata, 32, 1)
+    print(new_item.name + "'s price is: " + str(new_item.price) + ", it contains: " + new_item.desc)
     new_item.add_item()
+
+
+def test_get_item(item_id):
+    salad = get_item(item_id)
+
+    file_ops.writeBinaryFile('C:\\Users\\Naseem\\Desktop\\sala6a.png', salad.image)
+
+def test_update_item(item_id):
+    baguette = get_item(item_id)
+    baguette.desc = baguette.desc + " + 3ejel mix"
+    baguette.update_item()
+
+def test_get_items(business_id):
+    mylist = get_items(business_id)
+    for item in mylist:
+        print(item)
 
 # call here these functions to actually view/edit data in the DB.
 
-'''
-aa = DBItem("aaa", "aa", None, 100, 1)
-#print(aa)
-my_items = get_items(1)
-for item in my_items:
-    print(item)
-'''
-
+# test_get_items(1)
 
