61bd2899b7f142a60b13ec29de48cb1b1f144da4
Naseem Srour <naseemsr94@gmail.com>
2021-01-03 16:26:57 +0200

    Test POST item

--- server/tests/test_resources/test_item_api.py
+++ server/tests/test_resources/test_item_api.py
@@ -5,11 +5,11 @@ import json
 from flask.testing import FlaskClient
 from flask.wrappers import Response
 
-ITEMS_URL = "/items"
+ITEM_URL = "/item"
 
 def test_get_item(client: FlaskClient): 
-    # get an existing item
-    response: Response = client.get('/item/10')
+    # GET an existing item
+    response: Response = client.get(ITEM_URL + '/' + str(10))
     
     # make assertions 
     assert response.status_code == 200
@@ -17,9 +17,22 @@ def test_get_item(client: FlaskClient):
 
     # Get a non-existent item
     nonExistentID = 2
-    response_get_nonExist: Response = client.get('/item/' + str(nonExistentID))
+    response_get_nonExist: Response = client.get(ITEM_URL + '/' + str(nonExistentID))
 
-    # Assertions:
     assert response_get_nonExist.status_code == 404
     assert response_get_nonExist.json == json.loads(''' {"error": "Item with ID ''' + str(nonExistentID) + ''' was not found!"} ''')
+
+def test_post_item(client: FlaskClient):
+    # POST a new item
+    new_item = dict(item_name="NEW hummus", item_desc="3elbet hummus", price=15, business_id=1)
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': '*/*'
+    }
     
+    response: Response = client.post(ITEM_URL, data=json.dumps(new_item), headers=headers)
+    
+    assert response.status_code == 201
+    # assert response.content_type == mimetype
+    assert response.json == "Success! Item NEW hummus Created."  # Not an actual json, will switch it when we interface with the frontend later , as: {'success': 'item hummus added successfully'}
